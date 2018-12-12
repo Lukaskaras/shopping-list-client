23f51600c0c97a555693684f1bbfae036fb1466e
Lukas Karas <lukas.karas@iflix.com>
2018-12-12 20:30:26 -0100

    First components created

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="nav-wrapper red lighten-1">
+      <a href="#" className="brand-logo center">Logo
+      </a>
+    </nav>
+  )
+}
+
+export default Navbar
